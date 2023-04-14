22c586bb49ef0fa49123e79536b92be2e0dfcc66
abiyasa05 <abiyasap24@gmail.com>
2023-04-14 17:22:58 +0700

    Menampilkan Data Pemeriksaan

--- app/Models/Pemeriksaan.php
+++ app/Models/Pemeriksaan.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Pemeriksaan extends Model
 {
   use HasFactory;
+  protected $table = 'pemeriksaans';
+  protected $primaryKey = 'id_pemeriksaan';
+
+  protected $fillable = [
+    'id_pemeriksaan',
+    'id_ibu_hamil',
+    'tanggal',
+    'catatan'
+  ];
 
   public function ibu_hamil()
   {
