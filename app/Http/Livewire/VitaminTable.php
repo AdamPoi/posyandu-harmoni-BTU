7dc25242d2485175bc5434e94b465c32a37eb05b
Lenka <falendikapratama@gmail.com>
2023-04-28 16:10:09 +0700

    penambahan button pada vitamin

--- app/Http/Livewire/VitaminTable.php
+++ app/Http/Livewire/VitaminTable.php
@@ -22,8 +22,24 @@ class VitaminTable extends DataTableComponent
         ->sortable(),
       Column::make("Jenis vitamin", "jenis_vitamin")
         ->sortable(),
-      Column::make("Deskripsi", "deskripsi")
-        ->sortable(),
+        Column::make('Actions')
+            ->label(
+                function ($row) {
+                    $delete =
+                        '<a href="#" class="btn btn-icon icon-center btn-danger delete-btn" data-id="' . $row->id_vitamin . '">
+                            <i class="fas fa-trash"></i>
+                        </a>';
+                    $edit =
+                        '<a href="' . route('vitamin.edit', ['vitamin' => $row->id_vitamin]) . '" class="btn btn-icon icon-center btn-warning">
+                            <i class="fas fa-pencil-alt"></i>
+                        </a>';
+                    $detail =
+                        '<a href="' . route('vitamin.show', ['vitamin' => $row->id_vitamin]) . '" class="btn btn-icon icon-center btn-primary">
+                            <i class="fas fa-circle-info"></i>
+                        </a>';
+                    return $detail . $edit . $delete;
+                }
+            )->html(),
     ];
   }
 }
