46f432770e43938cb70e623ad4548e0bac35b0a0
percyfikri <mafikri27@gmail.com>
2023-04-28 10:57:35 +0700

    add views.index on Data Jadwal

--- resources/views/pages/jadwal/index.blade.php
+++ resources/views/pages/jadwal/index.blade.php
@@ -21,16 +21,16 @@
             </div>
             <div class="section-body">
                 @if (session('msg-success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
-                        <div class="alert-body">
-                            <div class="alert-title">Sukses</div>
-                            {{ session('msg-success') }}
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+                    <div class="alert-body">
+                        <div class="alert-title">Sukses</div>
+                        {{ session('msg-success') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
+                </div>
                 @endif
                 <div class="row">
                     <div class="col-12">
@@ -39,7 +39,8 @@
                                 <h4>Data Jadwal</h4>
                                 <div class="card-header-action">
 
-                                    <a href="#" class="btn btn-icon btn-primary icon-left"><i class="fas fa-plus"></i>
+                                    <a href="#" class="btn btn-icon btn-primary icon-left"><i
+                                            class="fas fa-plus"></i>
                                         Tambah</a>
 
                                 </div>
