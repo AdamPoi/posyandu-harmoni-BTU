cef32aed6085f1d6c39857ff26aaf79c3f418cf3
Lenka <falendikapratama@gmail.com>
2023-05-25 20:44:21 +0700

    Penambahan api register dan login dengan AuthController dan routes api untuk login dan register.

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
 {
   use HasFactory;
   use SoftDeletes;
+  use HasApiTokens;
+  use Notifiable;
 
   protected $table = 'users';
   protected $primaryKey = 'id_user';
@@ -39,5 +41,15 @@ class User extends Authenticatable
    */
   protected $hidden = [
     'password',
+    'remember_token',
+  ];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
   ];
 }
